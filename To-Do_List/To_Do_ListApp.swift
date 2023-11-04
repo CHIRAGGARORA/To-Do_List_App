443531da0ce56db0014f2de19bdaba8ced03f849
Chirag Arora <arora2002chirag@gmail.com>
2023-11-04 17:41:42 +0530

    Starting ToDo List app (Kalagato assignment)

---
+++ To-Do_List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To-Do_List
+//
+//  Created by chirag arora on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
